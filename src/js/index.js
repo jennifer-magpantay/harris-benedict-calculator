a896255698ec75df6decab09e918df048d2a0661
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2022-04-10 15:24:36 +0200

    Create script to handle menu and added other pages

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-console.log("first");
+// global selectors
+const navbarMenu = document.querySelector("#navbar-menu");
+const navButtonToggle = document.querySelector("#nav-button-toggle");
+
+// events
+navButtonToggle.addEventListener("click", handleMenu);
+
+// functions
+function handleMenu() {
+  // once button is clicked, display the navbar-menu, by setting data visible to true
+  const isVisible = navbarMenu.getAttribute("data-visible");
+  if (isVisible === "false") {
+    navbarMenu.setAttribute("data-visible", true);
+    // also make changes to aria-expanded
+    navButtonToggle.setAttribute("aria-expanded", true);
+  } else {
+    navbarMenu.setAttribute("data-visible", false);
+    navButtonToggle.setAttribute("aria-expanded", false);
+  }
+}
