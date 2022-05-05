a78ddbb5e02a43fe657eec4a4f86424c48beb5ae
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2022-05-05 15:17:47 +0200

    Set contentData for dynamic rendering

---
+++ src/data/contentData.js
@@ -0,0 +1,44 @@
+export const contentData = [
+  {
+    id: 1,
+    title: "What is the Daily Calorie calculator?",
+    description:
+      "It is a mathematical equation for estimating the total calories a person needs, known as Total Energy Expenditure(TEE), considering their Basal Metabolic Rate(BMR) and Physical Activities Level(PAL).",
+  },
+  {
+    id: 2,
+    title: " What is Basal Metabolic Rate (BMR)?",
+    description:
+      "It is the energy a person needs to carry out to complete fundamental metabolic functions, such as brain function or breath, in a period of time, without considering the energy that is spent on activities of the day.It is calculated by using the Harris- Benedict equation.",
+  },
+  {
+    id: 3,
+    title: "What is the Harris-Benedic Equation?",
+    description:
+      "It is an equation that takes into consideration height, weight, age and gender to estimate an individual's basal metabolic rate. The result of this equation multiplied by the PAL factor is used to calculate TEE.",
+  },
+  {
+    id: 4,
+    title: "What is Physical Activities Level (PAL)?",
+    description:
+      "PAL is a number multiplier used to estimate a person's TEE in combination with the BMR. It can be also used to compute the amount of food energy a person needs to consume in order to maintain a particular lifestyle.",
+  },
+  {
+    id: 5,
+    title: "Why do you need that?",
+    description:
+      "This is just a representation of a calculation that gives an estimative amount of calories an individual body would need per day based on a study.",
+  },
+  {
+    id: 6,
+    title: "FYI",
+    description:
+      "This application does not replace an appointment with your GP or your Nutritionist.",
+  },
+  {
+    id: 7,
+    title: "Important Advice",
+    description:
+      "If you are feeling any side effects from your day-to-day routine, for example, excessive tiredness, lack of sleep or apetite, or want to achieve a goal, such as gain or loose weight, you must contact a professional to understand what is going on with your health and to get the correct advice.",
+  },
+];
